f74e7cae3392bc426fc2c5c1a280a0c7ba3bfa5a
Cameron <cambboyle@gmail.com>
2025-10-07 18:33:09 +0100

    feat: Add Track logic with Track, Tracklist and Playlist components

---
+++ src/components/Playlist/Playlist.jsx
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import Tracklist from "../TrackList/Tracklist";
+
+const Playlist = (props) => {
+  const handleNameChange = useCallback(
+    (event) => {
+      props.onNameChange(event.target.value);
+    },
+    [props.onNameChange]
+  );
+  return (
+    <div className="Playlist">
+      <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+      <Tracklist
+        tracks={props.playlistTracks}
+        isRemoval={true}
+        onRemove={props.onRemove}
+      />
+      <button className="playlist-save" onClick={props.onSave}>
+        Save to Spotify
+      </button>
+    </div>
+  );
+};
+
+export default Playlist;
